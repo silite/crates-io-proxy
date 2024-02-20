7fcfceb88685c6a8398d16845bee72ec9da87a82
silite <lemnsilite@gmail.com>
2024-02-20 17:47:04 +0800

    use actix web

--- src/server.rs
+++ src/server.rs
@@ -1,8 +1,4 @@
-use axum::{
-    extract::{Path, State},
-    routing::get,
-    Json, Router, ServiceExt,
-};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder, Result};
 use once_cell::sync::Lazy;
 use serde_json::Value;
 use tokio::runtime::{Builder, Runtime};
@@ -22,48 +18,58 @@ pub static TOKIO_RUNTIME: Lazy<Runtime> = Lazy::new(|| {
         .unwrap()
 });
 
-pub fn start(conf: ProxyConfig) {
-    let app = Router::new()
-        .route("/index/config.json", get(config))
-        .route("/api/v1/crates/:name/:version/download", get(download))
-        .route("/index/:a/:b/:name", get(prefetch_crates))
-        .route("/index/:a/:name", get(prefetch_len2_crates))
-        .with_state(conf);
+// pub fn start(conf: ProxyConfig) {
+//     let app = Router::new()
+//         .route("/index/config.json", get(config))
+//         .route("/api/v1/crates/:name/:version/download", get(download))
+//         .route("/index/:a/:b/:name", get(prefetch_crates))
+//         .route("/index/:a/:name", get(prefetch_len2_crates))
+//         .with_state(conf);
 
-    TOKIO_RUNTIME.block_on(async {
-        let listener = tokio::net::TcpListener::bind("0.0.0.0:8888").await.unwrap();
-        axum::serve(listener, app.into_make_service())
-            .await
-            .unwrap();
+#[actix_web::main]
+pub async fn start(conf: ProxyConfig) {
+    let _ = TOKIO_RUNTIME.block_on(async {
+        HttpServer::new(move || {
+            App::new()
+                .app_data(web::Data::new(conf.clone()))
+                .route("/index/config.json", web::get().to(config))
+        })
+        .bind(("0.0.0.0", 8888))?
+        .run()
+        .await
     });
 }
 
-async fn config(State(conf): State<ProxyConfig>) -> Json<Value> {
-    Json(serde_json::from_str(&gen_config_json_file(&conf)).unwrap())
+async fn config(conf: web::Data<ProxyConfig>) -> Result<HttpResponse> {
+    let json_config = gen_config_json_file(&conf);
+    Ok(HttpResponse::Ok().json(json_config))
 }
+// async fn config(State(conf): State<ProxyConfig>) -> Json<Value> {
+//     Json(serde_json::from_str(&gen_config_json_file(&conf)).unwrap())
+// }
 
-async fn download(
-    Path((name, version)): Path<(String, String)>,
-    State(conf): State<ProxyConfig>,
-) -> Vec<u8> {
-    let crate_info = CrateInfo::new(&name, &version);
-    if let Some(data) = cache_fetch_crate(&conf.crates_dir, &crate_info) {
-        data
-    } else {
-        forward_download_request(crate_info, conf.clone())
-    }
-}
+// async fn download(
+//     Path((name, version)): Path<(String, String)>,
+//     State(conf): State<ProxyConfig>,
+// ) -> Vec<u8> {
+//     let crate_info = CrateInfo::new(&name, &version);
+//     if let Some(data) = cache_fetch_crate(&conf.crates_dir, &crate_info) {
+//         data
+//     } else {
+//         forward_download_request(crate_info, conf.clone())
+//     }
+// }
 
-async fn prefetch_crates(
-    Path((_a, _b, name)): Path<(String, String, String)>,
-    State(conf): State<ProxyConfig>,
-) -> Vec<u8> {
-    prefetch_with_name(&name, &conf).await
-}
+// async fn prefetch_crates(
+//     Path((_a, _b, name)): Path<(String, String, String)>,
+//     State(conf): State<ProxyConfig>,
+// ) -> Vec<u8> {
+//     prefetch_with_name(&name, &conf).await
+// }
 
-async fn prefetch_len2_crates(
-    Path((_a, name)): Path<(String, String)>,
-    State(conf): State<ProxyConfig>,
-) -> Vec<u8> {
-    prefetch_with_name(&name, &conf).await
-}
+// async fn prefetch_len2_crates(
+//     Path((_a, name)): Path<(String, String)>,
+//     State(conf): State<ProxyConfig>,
+// ) -> Vec<u8> {
+//     prefetch_with_name(&name, &conf).await
+// }
